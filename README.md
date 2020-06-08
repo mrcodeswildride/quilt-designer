565afa85d372579cb71d926484cfb13c73893ebc
David Keller <mrcodeswildride@gmail.com>
2020-06-08 13:29:51 +0000

    es6 update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # quilt-designer
 
-This is a quilt designer web app written in HTML, CSS, and JavaScript.
+Quilt designer web app written in HTML, CSS, and JavaScript.
 
 Use this app at [quiltdesigner.mrcodeswildride.com](https://quiltdesigner.mrcodeswildride.com/).
 Learn how to make this app at [mrcodeswildride.com](https://www.mrcodeswildride.com/).
--- script.js
+++ script.js
@@ -1,89 +1,75 @@
-var square1 = document.getElementById("square1");
-var square2 = document.getElementById("square2");
-var square3 = document.getElementById("square3");
-var square4 = document.getElementById("square4");
-var square5 = document.getElementById("square5");
-var square6 = document.getElementById("square6");
-var square7 = document.getElementById("square7");
-var square8 = document.getElementById("square8");
-var square9 = document.getElementById("square9");
+let square1 = document.getElementById(`square1`)
+let square2 = document.getElementById(`square2`)
+let square3 = document.getElementById(`square3`)
+let square4 = document.getElementById(`square4`)
+let square5 = document.getElementById(`square5`)
+let square6 = document.getElementById(`square6`)
+let square7 = document.getElementById(`square7`)
+let square8 = document.getElementById(`square8`)
+let square9 = document.getElementById(`square9`)
 
-var noBackgroundButton = document.getElementById("noBackground");
-var patternButton = document.getElementById("pattern");
-var boatButton = document.getElementById("boat");
-var dogButton = document.getElementById("dog");
-var selectedBackgroundParagraph = document.getElementById("selectedBackground");
+let patternButton = document.getElementById(`patternButton`)
+let boatButton = document.getElementById(`boatButton`)
+let dogButton = document.getElementById(`dogButton`)
+let imageParagraph = document.getElementById(`imageParagraph`)
 
-var rotation0Button = document.getElementById("rotation0");
-var rotation90Button = document.getElementById("rotation90");
-var rotation180Button = document.getElementById("rotation180");
-var rotation270Button = document.getElementById("rotation270");
-var selectedRotationParagraph = document.getElementById("selectedRotation");
+let rotation90Button = document.getElementById(`rotation90Button`)
+let rotation180Button = document.getElementById(`rotation180Button`)
+let rotation270Button = document.getElementById(`rotation270Button`)
+let rotationParagraph = document.getElementById(`rotationParagraph`)
 
-var selectedBackground = "";
-var selectedRotation = "";
+let selectedImage = ``
+let selectedRotation = ``
 
-square1.addEventListener("click", selectSquare);
-square2.addEventListener("click", selectSquare);
-square3.addEventListener("click", selectSquare);
-square4.addEventListener("click", selectSquare);
-square5.addEventListener("click", selectSquare);
-square6.addEventListener("click", selectSquare);
-square7.addEventListener("click", selectSquare);
-square8.addEventListener("click", selectSquare);
-square9.addEventListener("click", selectSquare);
+square1.addEventListener(`click`, changeSquare)
+square2.addEventListener(`click`, changeSquare)
+square3.addEventListener(`click`, changeSquare)
+square4.addEventListener(`click`, changeSquare)
+square5.addEventListener(`click`, changeSquare)
+square6.addEventListener(`click`, changeSquare)
+square7.addEventListener(`click`, changeSquare)
+square8.addEventListener(`click`, changeSquare)
+square9.addEventListener(`click`, changeSquare)
 
-noBackgroundButton.addEventListener("click", setNoBackground);
-patternButton.addEventListener("click", setPattern);
-boatButton.addEventListener("click", setBoat);
-dogButton.addEventListener("click", setDog);
+patternButton.addEventListener(`click`, selectPattern)
+boatButton.addEventListener(`click`, selectBoat)
+dogButton.addEventListener(`click`, selectDog)
 
-rotation0Button.addEventListener("click", setRotation0);
-rotation90Button.addEventListener("click", setRotation90);
-rotation180Button.addEventListener("click", setRotation180);
-rotation270Button.addEventListener("click", setRotation270);
+rotation90Button.addEventListener(`click`, select90)
+rotation180Button.addEventListener(`click`, select180)
+rotation270Button.addEventListener(`click`, select270)
 
-function selectSquare() {
-    this.style.backgroundImage = selectedBackground ? 'url("' + selectedBackground + '")' : "";
-    this.style.transform = selectedRotation ? "rotate(" + selectedRotation + "deg)" : "";
+function changeSquare() {
+  this.style.backgroundImage = `url("${selectedImage}")`
+  this.style.transform = `rotate(${selectedRotation})`
 }
 
-function setNoBackground() {
-    selectedBackground = "";
-    selectedBackgroundParagraph.innerHTML = "Selected image: none";
+function selectPattern() {
+  selectedImage = `pattern.jpg`
+  imageParagraph.innerHTML = `Selected image: pattern`
 }
 
-function setPattern() {
-    selectedBackground = "pattern.jpg";
-    selectedBackgroundParagraph.innerHTML = "Selected image: pattern";
+function selectBoat() {
+  selectedImage = `boat.jpg`
+  imageParagraph.innerHTML = `Selected image: boat`
 }
 
-function setBoat() {
-    selectedBackground = "boat.jpg";
-    selectedBackgroundParagraph.innerHTML = "Selected image: boat";
+function selectDog() {
+  selectedImage = `dog.jpg`
+  imageParagraph.innerHTML = `Selected image: dog`
 }
 
-function setDog() {
-    selectedBackground = "dog.jpg";
-    selectedBackgroundParagraph.innerHTML = "Selected image: dog";
+function select90() {
+  selectedRotation = `90deg`
+  rotationParagraph.innerHTML = `Selected rotation: 90 degrees`
 }
 
-function setRotation0() {
-    selectedRotation = 0;
-    selectedRotationParagraph.innerHTML = "Selected rotation: 0 degrees";
+function select180() {
+  selectedRotation = `180deg`
+  rotationParagraph.innerHTML = `Selected rotation: 180 degrees`
 }
 
-function setRotation90() {
-    selectedRotation = 90;
-    selectedRotationParagraph.innerHTML = "Selected rotation: 90 degrees";
-}
-
-function setRotation180() {
-    selectedRotation = 180;
-    selectedRotationParagraph.innerHTML = "Selected rotation: 180 degrees";
-}
-
-function setRotation270() {
-    selectedRotation = 270;
-    selectedRotationParagraph.innerHTML = "Selected rotation: 270 degrees";
+function select270() {
+  selectedRotation = `270deg`
+  rotationParagraph.innerHTML = `Selected rotation: 270 degrees`
 }
